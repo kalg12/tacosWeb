9c52842609a54f0c2b6261716746aea7424bc444
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-07-05 11:21:10 -0600

    Subiendo mi primer página.

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('showRecipe').addEventListener('click', function() {
+    var recipe = document.getElementById('recipe');
+    if (recipe.style.display === 'none') {
+        recipe.style.display = 'block';
+    } else {
+        recipe.style.display = 'none';
+    }
+});
